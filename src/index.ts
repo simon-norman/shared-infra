1e5ba46398ebee18f5d2cdf8ce779b9d9d87e8ee
simon-personal <01secn@gmail.com>
2024-02-04 11:52:07 +0000

    ts config and index files

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * from 'src/components/digital-ocean/vpn'
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   target: 'esnext',
   format: ['cjs'],
+  entry: ['src/index.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
