3d9e3bf331d29c8216c40d7822550b0688093e4b
simon-personal <01secn@gmail.com>
2024-02-04 12:52:28 +0000

    created default vpc from digital ocean

--- src/components/digital-ocean/vpn.ts
+++ src/components/digital-ocean/vpn.ts
@@ -1,19 +1,16 @@
 import * as pulumi from "@pulumi/pulumi"
 import * as digitalocean from "@pulumi/digitalocean";
 
-class Vpn extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions) {
+export class Vpc extends pulumi.ComponentResource {
+    vpc: digitalocean.Vpc
+
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("shared-infra:digital-ocean:Vpn", name, {}, opts);
 
-        const defaultVpn = new digitalocean.Vpc(name, {
-            ipRange: "10.10.10.0/24",
-            region: "nyc3",
+        this.vpc = new digitalocean.Vpc(name, {
+            region: "lon1",
         }, { parent: this });
-
-
-    
 
         this.registerOutputs();
     }
 }
-
